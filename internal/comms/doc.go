3369071aa7d92b526cf5e52310b34a1396ab6657
Corey Wagehoft <corey.wagehoft@gmail.com>
2026-02-26 17:28:58 -0700

    fix: remove build tag

--- internal/comms/doc.go
+++ internal/comms/doc.go
@@ -1,5 +1,3 @@
-//go:build omd_omit_comms
-
 // Package comms provides PTT (push-to-talk) communications using pion-based
 // RTP/RTCP. Build with -tags omd_omit_comms to exclude this package.
 package comms
